f3800bf4e7fb1a161f0430978e5039e3f9f7c77c
Yoon Kim <jyk416@ic.ac.uk>
2020-06-16 23:00:33 +0900

    Trial of progress bar

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 import streamlit as st
 import numpy as np
+import time
 
-progress_bar = st.sidebar.progress(0)
-frame_text = st.sidebar.empty()
-
-for frame_num, a in enumerate(np.linspace(0.0, 4 * np.pi, 100)):
-    # Here were setting value for these two elements.
-    progress_bar.progress(frame_num)
-    frame_text.text("Frame %i/100" % (frame_num + 1))
+model_bar = st.progress(0)
+for p in range(100):
+    model_bar.progress(p + 1)
