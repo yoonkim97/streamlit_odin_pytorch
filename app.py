82e4a4e62db8fc050b1df2044548de3c6ed655d0
Yoon Kim <jyk416@ic.ac.uk>
2020-06-16 22:57:33 +0900

    Trial with model load numbers

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 import streamlit as st
-st.title("Streamlit Title")
+import numpy as np
+
+progress_bar = st.sidebar.progress(0)
+frame_text = st.sidebar.empty()
+
+for frame_num, a in enumerate(np.linspace(0.0, 4 * np.pi, 100)):
+    # Here were setting value for these two elements.
+    progress_bar.progress(frame_num)
+    frame_text.text("Frame %i/100" % (frame_num + 1))
